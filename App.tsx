7071197da24d085c7450f193735a33625668ea7d
DonTrozo <trozo.antonio@gmail.com>
2026-06-30 22:22:14 +0200

    Open interactive product shell directly on web

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { ProductEntry } from './src/ProductEntry';
 import { ModeShell } from './src/screens/ModeShell';
 
 export default function App() {
-  return (
-    <ProductEntry>
-      <ModeShell />
-    </ProductEntry>
-  );
+  return <ModeShell />;
 }
